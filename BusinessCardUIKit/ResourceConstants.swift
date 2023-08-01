fa56b04f27098eca874cf7426a2893aba1b3c13d
Vl-VSV <vvs_2015@icloud.com>
2023-08-01 22:42:11 +0300

    Complete

---
+++ BusinessCardUIKit/ResourceConstants.swift
@@ -0,0 +1,21 @@
+//
+//  ResourceConstants.swift
+//  BusinessCardUIKit
+//
+//  Created by Vlad V on 01.08.2023.
+//
+
+import UIKit
+
+// MARK: - Color Palette
+enum ColorPallete {
+    static let background = UIColor(named: "Background")
+    static let tertiary = UIColor(named: "Tertiary")
+}
+
+enum ImageAssets {
+    static let profile = UIImage(named: "Profile")
+    static let pencil = UIImage(named: "Pencil")
+    static let mappin = UIImage(named: "Mappin")
+    static let checkmark = UIImage(named: "Checkmark")
+}
