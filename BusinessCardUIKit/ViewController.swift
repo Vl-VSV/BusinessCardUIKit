d6020bdbd81caaf32eb7c53e035586b62cf468b1
Vl-VSV <vvs_2015@icloud.com>
2023-08-01 10:29:07 +0300

    Initial Commit

---
+++ BusinessCardUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusinessCardUIKit
+//
+//  Created by Vlad V on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
